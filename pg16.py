a3b75384d8cdb4db2476d780793956e539114f4a
Nasreen98 <47441395+Nasreen98@users.noreply.github.com>
2019-02-18 19:12:40 +0530

    Create pg16.py

---
+++ pg16.py
@@ -0,0 +1,10 @@
+a,b=raw_input().split()
+a=int(a)
+b=int(b)
+for num in range(a+1,b):
+   if num > 1:
+      for i in range(2,num):
+          if (num % i == 0):
+              break
+      else:
+          print(num),
